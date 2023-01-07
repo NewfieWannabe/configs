ce206fefb701a93b6ba978dcdd6b20dc4497d2cd
NewfieWannabe <NewfieWannabe@icloud.com>
2023-01-06 21:50:30 -0700

    Brew formulae to install

---
+++ scripts/brews.sh
@@ -0,0 +1,19 @@
+# Command to install various brew scripts I use
+# Last updated 18 Nov 2022 0735
+
+
+# Brew update
+brew --update
+brew --upgrade
+
+# TVNamer
+brew install tvnamer
+
+# duf
+brew insttall duf
+
+# htop
+brew install htop
+
+# exa
+brew install exa
