d0e7c7484bd742c735e8c83621b46dcf94305a34
NewfieWannabe <NewfieWannabe@icloud.com>
2023-04-09 14:20:08 -0600

    More utils, code cleanup

--- scripts/brews.sh
+++ scripts/brews.sh
@@ -1,19 +1,24 @@
 # Command to install various brew scripts I use
-# Last updated 230308-1313
+# Last updated 240409-1419
 
 
 # Brew update
 brew update
 brew upgrade
 
-# TVNamer
+# Python
+brew install python@3.10 python@3.11
+
+# OpenJDK
+brew install openjdk
+
+# Command-line tools
+brew install cmake
+brew install fing
 brew install tvnamer
-
-# duf
 brew install duf
-
-# htop
 brew install htop
-
-# exa
 brew install exa
+brew install rar
+brew install wget
+
