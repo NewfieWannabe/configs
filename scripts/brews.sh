6dc5a1e56b1cdbeadd807ef9a68dc249d78fee2c
NewfieWannabe <NewfieWannabe@icloud.com>
2023-09-20 13:11:37 -0600

    Updated Exa to Eza

--- scripts/brews.sh
+++ scripts/brews.sh
@@ -1,5 +1,5 @@
 # Command to install various brew scripts I use
-# Last updated 240409-1419
+# Last updated 20 Sep 2023 @ 1310
 
 
 # Brew update
@@ -18,7 +18,7 @@ brew install fing-cli fing
 brew install tvnamer
 brew install duf
 brew install htop
-brew install exa
+brew install eza
 brew install rar
 brew install wget
 
