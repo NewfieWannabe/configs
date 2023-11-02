ea4b50dd85b587fd6e015cbccd5512b3976792b5
NewfieWannabe <NewfieWannabe@icloud.com>
2023-11-02 22:39:14 +0000

    Updated name

---
+++ scripts/gits.mac.sh
@@ -0,0 +1,8 @@
+# Git clones to do on Mac
+
+git clone git@github.com:newfiewannabe/configs config
+git clone git@github.com:vim/vim vim
+git clone git@github.com:python/cpython python3
+git clone git@github.com:diasurgical/devilutionx devilutionx
+git clone git@github.com:brofield/simpleini simpleini
+git clone git@github.com:mpartel/bindfs bindfs
