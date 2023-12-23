40069979b6334485529e371810e5bcf2c4f5a9f3
NewfieWannabe <NewfieWannabe@icloud.com>
2023-12-23 12:19:45 -0700

    Updated for beeper

--- scripts/gits.mac.sh
+++ scripts/gits.mac.sh
@@ -1,11 +1,12 @@
 # Git clones to do on Mac
+# Last updated 23 December 2023 @ 1219
 
-git clone git@github.com:newfiewannabe/configs          config
-git clone git@github.com:vim/vim                        vim
-git clone git@github.com:python/cpython                 python3
-git clone git@github.com:diasurgical/devilutionx        devilutionx
-git clone git@github.com:brofield/simpleini             simpleini
-git clone git@github.com:mpartel/bindfs                 bindfs
-git clone git@github.com:eza-community/eza              eza
-git clone https://git.uupdump.net/uup-dump/converter    uupdump
-git clone beeper/mac-registration-provider              beeper-mac
+git clone git@github.com:newfiewannabe/configs                  config
+git clone git@github.com:vim/vim                                vim
+git clone git@github.com:python/cpython                         python3
+git clone git@github.com:diasurgical/devilutionx                devilutionx
+git clone git@github.com:brofield/simpleini                     simpleini
+git clone git@github.com:mpartel/bindfs                         bindfs
+git clone git@github.com:eza-community/eza                      eza
+git clone https://git.uupdump.net/uup-dump/converter            uupdump
+git clone git@github.com:beeper/mac-registration-provider       beeper-mac
