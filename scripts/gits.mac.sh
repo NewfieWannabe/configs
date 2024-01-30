ac1f9ea29be98681801e34c4c5671cd36ecd370f
NewfieWannabe <NewfieWannabe@icloud.com>
2024-01-29 19:56:41 -0700

    Removed Beeper

--- scripts/gits.mac.sh
+++ scripts/gits.mac.sh
@@ -1,5 +1,5 @@
 # Git clones to do on Mac
-# Last updated 23 December 2023 @ 1219
+# Last updated 29 January 2024 @ 1956
 
 git clone git@github.com:newfiewannabe/configs                  config
 git clone git@github.com:vim/vim                                vim
@@ -9,4 +9,3 @@ git clone git@github.com:brofield/simpleini                     simpleini
 git clone git@github.com:mpartel/bindfs                         bindfs
 git clone git@github.com:eza-community/eza                      eza
 git clone https://git.uupdump.net/uup-dump/converter            uupdump
-git clone git@github.com:beeper/mac-registration-provider       beeper-mac
