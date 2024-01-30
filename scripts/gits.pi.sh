93dc54ae913b510864065aa68875610296b1f215
NewfieWannabe <NewfieWannabe@icloud.com>
2024-01-30 03:02:00 +0000

    Updated for eza

--- scripts/gits.pi.sh
+++ scripts/gits.pi.sh
@@ -4,3 +4,4 @@ git clone git@github.com:newfiewannabe/configs      config
 git clone git@github.com:vim/vim                    vim
 git clone git@github.com:python/cpython             python3
 git clone git@github.com:muesli/duf                 duf
+git clone git@github.com:eza-community/eza          eza
